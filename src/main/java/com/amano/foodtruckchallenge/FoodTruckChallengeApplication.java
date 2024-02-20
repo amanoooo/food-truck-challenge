c4cabf36067848b7ae67bc75ecf2b8e6ed700200
amano <1060996790@qq.com>
2024-02-20 18:07:00 +0800

    fix run error, add log

--- src/main/java/com/amano/foodtruckchallenge/FoodTruckChallengeApplication.java
+++ src/main/java/com/amano/foodtruckchallenge/FoodTruckChallengeApplication.java
@@ -1,9 +1,11 @@
 package com.amano.foodtruckchallenge;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.amano.foodtruckchallenge.mapper")
 public class FoodTruckChallengeApplication {
 
 	public static void main(String[] args) {
