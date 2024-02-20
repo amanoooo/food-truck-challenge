434e352402320f815f54afeb1a6c2bc67384de13
amano <1060996790@qq.com>
2024-02-20 18:04:25 +0800

    add food csv,  init script

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,20 @@
-INSERT INTO user (name, phone, age)
-VALUES ('amano', '15021170251', 18),
-       ('hikaru', '15021170251', 17);
-
-
+INSERT INTO `mobile_food_info`(`locationid`,
+                               `applicant`,
+                               `facility_type`,
+                               `cnn`,
+                               `location_description`,
+                               `address`,
+                               `permit`,
+                               `status`,
+                               `food_items`)
+SELECT locationid,
+       Applicant,
+       FacilityType,
+       cnn,
+       LocationDescription,
+       Address,
+       permit,
+       Status,
+       FoodItems
+FROM CSVREAD('./Mobile_Food_facility_Permit.csv')
+;
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,14 +1,19 @@
-DROP TABLE IF EXISTS `user`;
-
-CREATE TABLE `user`
+DROP TABLE IF EXISTS `mobile_food_info`;
+CREATE TABLE `mobile_food_info`
 (
-    `id`         bigint    NOT NULL AUTO_INCREMENT COMMENT '主键ID',
-    `name`       varchar(255)       DEFAULT NULL COMMENT '姓名',
-    `phone`      varchar(20)        DEFAULT NULL COMMENT '头像',
-    `age`        int                DEFAULT NULL COMMENT '年龄',
-    `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-    `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
-    `deleted`    char(1)   NOT NULL DEFAULT '0' COMMENT '逻辑删除 1已删',
-    PRIMARY KEY (`id`),
-    KEY          `phone_idx` (`phone`)
+    `id`                   bigint    NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+    `locationid`           bigint             DEFAULT NULL COMMENT '位置ID',
+    `applicant`            varchar(255)        DEFAULT NULL COMMENT '申请人',
+    `facility_type`        varchar(20)        DEFAULT NULL COMMENT '设施类型',
+    `cnn`                  varchar(20)        DEFAULT NULL COMMENT 'cnn',
+    `location_description` varchar(255)       DEFAULT NULL COMMENT '位置描述',
+    `address`              varchar(255)       DEFAULT NULL COMMENT '地址',
+    `permit`               varchar(20)        DEFAULT NULL COMMENT '允许',
+    `status`               varchar(20)        DEFAULT NULL COMMENT '状态',
+    `food_items`           text               DEFAULT NULL COMMENT '食品',
+
+    `created_at`           timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `updated_at`           timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+    `deleted`              char(1)   NOT NULL DEFAULT '0' COMMENT '逻辑删除 1已删',
+    PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
